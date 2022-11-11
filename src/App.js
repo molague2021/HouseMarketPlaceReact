4070b79e1f46984cddc3dc3d19bbb5c00e4f835c
Martin Olague <molague23@hotmail.com>
2022-11-10 22:05:23 -0700

    Alerts with React Toastify

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Navbar from './components/Navbar';
 import Explore from './pages/Explore';
 import ForgotPassword from './pages/ForgotPassword';
@@ -22,6 +24,8 @@ function App() {
         {/* Navbar*/}
         <Navbar />
       </Router>
+
+      <ToastContainer />
     </>
   );
 }
