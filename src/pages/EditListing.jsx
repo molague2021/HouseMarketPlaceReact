4b8725571bfe6b5c2e8af3395d7d89c192c94dea
Martin Olague <molague23@hotmail.com>
2023-06-08 16:36:53 -0700

    Added edit listing page and route

---
+++ src/pages/EditListing.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const EditListing = () => {
+  return <div>EditListing</div>;
+};
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -101,6 +101,10 @@ function Profile() {
     }
   };
 
+  const onEdit = async (listingId) => {
+    navigate(`/edit-listing/${listingId}`);
+  };
+
   return (
     <div className="profile">
       <header className="profileHeader">
@@ -160,6 +164,7 @@ function Profile() {
                   listing={listing.data}
                   id={listing.id}
                   onDelete={() => onDelete(listing.id)}
+                  onEdit={() => onEdit(listing.id)}
                 />
               ))}
             </ul>
